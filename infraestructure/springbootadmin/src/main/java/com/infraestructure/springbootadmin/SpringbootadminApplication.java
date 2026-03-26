e76968bb192cf4f32bb24dd164da163c8670145f
jmiwassjukourens <jiwassjuk@gmail.com>
2026-03-26 12:58:34 -0300

    admin annotations added

--- infraestructure/springbootadmin/src/main/java/com/infraestructure/springbootadmin/SpringbootadminApplication.java
+++ infraestructure/springbootadmin/src/main/java/com/infraestructure/springbootadmin/SpringbootadminApplication.java
@@ -1,9 +1,16 @@
 package com.infraestructure.springbootadmin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringbootadminApplication {
 
 	public static void main(String[] args) {
